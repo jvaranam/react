036b5e8d74a4a5c0a8d8aa05c6232a0f632d81a1
Jose Varanam <jvaranam@gmail.com>
2017-06-05 17:03:27 +0530

    google authentication

---
+++ googleAuthentication/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './style/lib/bootstrap/css/bootstrap.min.css';
+import './style/lib/font-awesome/css/font-awesome.min.css';
+import GoogleLogin from 'react-google-login';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+
+        // This binding is necessary to make `this` work in the callback
+        //this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(_evt) {
+        /*this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));*/
+        console.log("alert event " + _evt)
+    }
+
+
+    render() {
+        return ( < div className = "App" >
+            < div className = "App-header" >
+            < img src = { logo }
+            className = "App-logo"
+            alt = "logo" / >
+            < h2 > Welcome to IO - Media Virtual Venue VR QC Tool v .0 .0 .1 < /h2> < /div > < div className = "App-intro"
+            id = "login" >
+            < GoogleLogin clientId = "896021468508-1ktndsomsnev4chqpg6578mufmeomumf.apps.googleusercontent.com"
+            buttonText = "Login" hd="io-media.com"
+            onSuccess = { responseGoogle }
+            onFailure = { responseGoogle }
+            hostedDomain= "io-media.com"
+            />  < /div > < /div>
+
+        );
+    }
+
+
+}
+
+const responseGoogle = (response) => {
+  if(response.getHostedDomain() != "io-media.com"){
+    response.disconnect()
+  }
+  console.log(response);
+}
+
+export default App;
